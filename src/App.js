60a3c612f7f833727e61e73e13e2fbf4e5cc0521
birdaNFT <birdaNFT@gmail.com>
2022-11-12 20:50:43 +0000

    Add sidebar section

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./App.css";
 import Header from "./Header";
+import Sidebar from "./Sidebar";
 
 function App() {
   return (
@@ -8,9 +9,12 @@ function App() {
       <Header />
 
       {/* App Body */}
-      {/* - Sidebar */}
-      {/* - Feed */}
-      {/* - Widgets */}
+      <div className="app__body">
+        <Sidebar />
+        {/* - Sidebar */}
+        {/* - Feed */}
+        {/* - Widgets */}
+      </div>
     </div>
   );
 }
