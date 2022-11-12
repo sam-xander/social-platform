439da202b46e6aba8d9a0a4e0f19fecb06229033
birdaNFT <birdaNFT@gmail.com>
2022-11-12 18:59:07 +0000

    added header options

--- src/Header.js
+++ src/Header.js
@@ -1,6 +1,13 @@
 import React from "react";
 import "./Header.css";
 import SearchIcon from "@mui/icons-material/Search";
+import HomeIcon from "@mui/icons-material/Home";
+import HeaderOption from "./HeaderOption";
+
+import SupervisorAccountIcon from "@mui/icons-material/SupervisorAccount";
+import BusinessCenterIcon from "@mui/icons-material/BusinessCenter";
+import ChatIcon from "@mui/icons-material/Chat";
+import NotificationsIcon from "@mui/icons-material/Notifications";
 
 function Header() {
   return (
@@ -15,7 +22,19 @@ function Header() {
           <input type="text" />
         </div>
       </div>
-      <div className="header__right"></div>
+      <div className="header__right">
+        <HeaderOption Icon={HomeIcon} title="Home" />
+        <HeaderOption Icon={SupervisorAccountIcon} title="My Network" />
+        <HeaderOption Icon={BusinessCenterIcon} title="Jobs" />
+        <HeaderOption Icon={ChatIcon} title="Messaging" />
+        <HeaderOption Icon={NotificationsIcon} title="Notifications" />
+        <HeaderOption
+          avatar={
+            "https://compassionate-leakey-e9b16b.netlify.app/images/IG_Sonny.jpeg"
+          }
+          title="Me"
+        />
+      </div>
     </header>
   );
 }
