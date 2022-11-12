3eac700229ae3e99d4680fa621a1aa3a1f7196c1
birdaNFT <birdaNFT@gmail.com>
2022-11-12 17:47:13 +0000

    add header section

---
+++ src/Header.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./Header.css";
+import SearchIcon from "@mui/icons-material/Search";
+
+function Header() {
+  return (
+    <header>
+      <div className="header__left">
+        <img
+          src="https://cdn-icons-png.flaticon.com/512/174/174857.png"
+          alt=""
+        />
+        <div className="header__search">
+          <SearchIcon />
+          <input type="text" />
+        </div>
+      </div>
+      <div className="header__right"></div>
+    </header>
+  );
+}
+
+export default Header;
