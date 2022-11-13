f132cb29bff2d35e9e2509f3d60b77a264d89d94
birdaNFT <birdaNFT@gmail.com>
2022-11-13 18:26:09 +0000

    Integrated redux

--- src/App.js
+++ src/App.js
@@ -1,19 +1,28 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import "./App.css";
+import { selectUser } from "./features/userSlice";
 import Feed from "./Feed";
 import Header from "./Header";
+import Login from "./Login";
 import Sidebar from "./Sidebar";
 
 function App() {
+  const User = useSelector(selectUser);
+
   return (
     <div className="app">
       <Header />
 
-      <div className="app__body">
-        <Sidebar />
-        <Feed />
-        {/* - Widgets */}
-      </div>
+      {!User ? (
+        <Login />
+      ) : (
+        <div className="app__body">
+          <Sidebar />
+          <Feed />
+          {/* - Widgets */}
+        </div>
+      )}
     </div>
   );
 }
